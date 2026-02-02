15a28e628cfafd3ceffbf367fd272f367ae8f8b4
Scaveng3r <128704976+Scaveng3rZero@users.noreply.github.com>
2026-02-01 19:13:12 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
+$(function () {
+
+  // Hamburger menu toggle (mobile)
+  $(".hamburger").on("click", function () {
+    const expanded = $(this).attr("aria-expanded") === "true";
+    $(this).attr("aria-expanded", String(!expanded));
+    $(".nav").toggleClass("open");
+  });
+
+  // Menu tabs (DRINKS / FOOD)
+  $(".menu-tab").on("click", function () {
+    $(".menu-tab")
+      .removeClass("active")
+      .attr("aria-selected", "false");
+
+    $(this)
+      .addClass("active")
+      .attr("aria-selected", "true");
+
+    const target = $(this).data("target");
+    $(".menu-panel").addClass("is-hidden");
+    $(target).removeClass("is-hidden");
+
+    $("html, body").animate(
+      { scrollTop: $(".menu-title").offset().top - 10 },
+      200
+    );
+  });
+
+});
 
